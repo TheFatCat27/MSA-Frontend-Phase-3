affca52f5558516679e7531092407463259daa44
TheFatCat <74298130+TheFatCat27@users.noreply.github.com>
2021-08-09 21:51:39 +1200

    Second Commit?

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Nav } from './Navbar.elements';
+
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                testing
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
